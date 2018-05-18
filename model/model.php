40a06978eca7ce02fa5de3c8269aa857918c8b0e
NJacq <nicolas.j@codeur.online>
2018-05-18 09:59:42 +0200

    c

--- model/model.php
+++ model/model.php
@@ -6,7 +6,7 @@ function getImages()
 
 $bdd = new PDO('mysql:host=localhost;dbname=generator_meme;charset=utf8' ,'root','');
 
-$response = $bdd->prepare("SELECT adress FROM img");
+$response = $bdd->prepare("SELECT adress,id FROM img");
 $response->fetchAll(PDO::FETCH_ASSOC);
 $response->execute();
 return $response;
--- view/choixbddView.php
+++ view/choixbddView.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
  
@@ -15,17 +15,17 @@
     <img class="logo" src="../assets/media/logo.png"> 
 </div>
 
- <div class="container">
-<div class="row">
-<div class="col-12 d-flex justify-content-around">
-<img src="../assets/media/cercle1.jpg" class="rounded-circle" />
-<img src="../assets/media/cercle2blanc.png" class="rounded-circle" />
-<img src="../assets/media/cercle3blanc.png" class="rounded-circle" />
-</div>
-</div>
+<div class="container">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-around">
+            <img src="../assets/media/cercle1.jpg" class="rounded-circle" />
+            <img src="../assets/media/cercle2blanc.png" class="rounded-circle" />
+            <img src="../assets/media/cercle3blanc.png" class="rounded-circle" />
+        </div>
+    </div>
 </div> 
 
-<div>
+<div class="list">
 
 <?php
 
@@ -39,10 +39,10 @@ foreach($result as $row){
 ?>
 
 <div class="images">
-<p class="img"><img src="../<?= $row["adress"]?>"witdh="100" height="100"></p>
+    <p class="img"><img data-id="<?= $row["id"]?>" src="../<?= $row["adress"]?>"style="cursor:pointer" witdh="300" height="300"></p>
 </div>
 
-
+ 
 <?php
 
   }
@@ -51,8 +51,23 @@ foreach($result as $row){
 
   ?>
 
-<input type="button" class="btn btn-info" value="Valider" onclick="location.href = 'memegeneView.php';">
-<input type="button" class="btn btn-info" value="Retour" onclick="location.href = 'choixView.php';">
+  
 
+<p id="list"></p>  
+<form name="inscription" method="post" action="memegeneView.php">
+<div>
+<input id="choiceImg" type="hidden" name="id" value="<?= $row["id"]?>">
+<button type="submit"  onclick="location.href = 'memegeneView.php';">Try it</button>
+    <input type="button" class="btn btn-info" value="Valider" onclick="location.href = 'memegeneView.php';">
+    <input type="button" class="btn btn-info" value="Retour" onclick="location.href = 'choixView.php';">
+</div>
+
+<script>
+   function myFunction() {
+        var x = document.getElementById("choiceImg").onclick=getAttribute("value");
+    document.getElementById("list").innerHTML = x;
+}
+</script>
+</form>
 </body>
 </html>
