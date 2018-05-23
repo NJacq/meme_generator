c6aff92bc183c8feadc625f304826ca08e4e8193
chaymaa <chayma_attia@hotmail.fr>
2018-05-23 11:22:01 +0200

    twig

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+//twig: copier coller de la documentation de twig pour developpeurs
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('view');
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+));
+
+$template = $twig->load('accueilView.html');
+echo $template->render(array());
