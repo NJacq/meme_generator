6395eb7af6c7ee9e60e8c904c0b723a85c705f06
NJacq <nicolas.j@codeur.online>
2018-05-23 11:17:46 +0200

    htaccess controller de accueil

--- controller/memegen.php
+++ controller/memegen.php
@@ -1,20 +1,18 @@
 <?php
 
 session_start();
+
 $_SESSION['choixImage']=$_GET['id'];
-//var_dump($_SESSION['choixImage']);
-require ('../model/model.php');
+require ('model/model.php');
+
 $image=getImage($_SESSION['choixImage']);
 $url=$image['adress'];
 $img_choice = $image["adress"];
 
 
+$imageView = getImage($_SESSION['choixImage']);
 
-       $imageView = getImage($_SESSION['choixImage']);
-echo "coucou";
-var_dump($imageView);
-
-    require('../view/memegeneView.php');
+require('view/memegeneView.php');
 
 
 
--- view/accueilView.php
+++ view/accueilView.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
@@ -12,15 +12,15 @@
 </head>
 <body>
     <div class="navbar">
-    <img class="logo" src="../assets/media/logo.png">
+    <img class="logo" src="assets/media/logo.png">
 </div>
-<p><img src="../assets/media/exemple_meme/ex-meme.jpg"></p>
+<p><img src="assets/media/exemple_meme/ex-meme.jpg"></p>
 <div class="exempleImg"></div>
-<input type="button" class="btn btn-info" value="Création du même" onclick="location.href = 'choixView.php';">
+<input type="button" class="btn btn-info" value="Création du même" onclick="location.href = 'choixView';">
 
 <h2>Gallerie des derniers mêmes crées :</h2>
-<img src="../assets/media/memes_crees/meme1.jpg">
-<img src="../assets/media/memes_crees/meme2.jpg">
+<img src="assets/media/memes_crees/meme1.jpg">
+<img src="assets/media/memes_crees/meme2.jpg">
 <!-- <img src="../assets/media/memes_crees/meme3.jpg"> -->
 
 
